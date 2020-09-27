ef59d9d622663fcafb3193690efae6f5ee60068c
Lars B <battla@protonmail.com>
2020-09-27 20:53:17 +0200

    Added Test Classes for statistic package

---
+++ src/jTest/CountTest.java
@@ -0,0 +1,26 @@
+package jTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReport() {
+    }
+}
